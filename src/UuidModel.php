be6ef3ef7b15ab6d897a57fe9b113c2d8cd8535a
michalsn <michal@sniatala.pl>
2020-04-27 16:24:34 +0200

    basic uuid requirements

--- src/UuidModel.php
+++ src/UuidModel.php
@@ -1,43 +1,8 @@
 <?php
 
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014-2019 British Columbia Institute of Technology
- * Copyright (c) 2019-2020 CodeIgniter Foundation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package    CodeIgniter
- * @author     CodeIgniter Dev Team
- * @copyright  2019-2020 CodeIgniter Foundation
- * @license    https://opensource.org/licenses/MIT	MIT License
- * @link       https://codeigniter.com
- * @since      Version 4.0.0
- * @filesource
- */
+namespace Michalsn\UuidModel;
 
-namespace CodeIgniter;
+use Ramsey\Uuid\Uuid;
 
 use Closure;
 use CodeIgniter\Database\BaseBuilder;
@@ -55,25 +20,30 @@ use ReflectionProperty;
 use stdClass;
 
 /**
- * Class Model
- *
- * The Model class provides a number of convenient features that
- * makes working with a database table less painful.
- *
- * It will:
- *      - automatically connect to database
- *      - allow intermingling calls between db connection, the builder,
- *          and methods in this class.
- *      - simplifies pagination
- *      - removes the need to use Result object directly in most cases
- *      - allow specifying the return type (array, object, etc) with each call
- *      - ensure validation is run against objects when saving items
- *
- * @package CodeIgniter
- * @mixin   BaseBuilder
+ * Uuid Class Model is based on a Model shipped with CodeIgniter
  */
-class Model
+class UuidModel
 {
+	/**
+	 * Used UUID version.
+	 *
+	 * @param string
+	 */
+	protected $uuidVersion = 'uuid1';
+
+	/**
+	 * Store UUID in byte format.
+	 *
+	 * @param boolean
+	 */
+	protected $uuidUseBytes = false;
+
+	/**
+	 * UUID fields.
+	 *
+	 * @param string
+	 */
+	protected $uuidFields = ['id'];
 
 	/**
 	 * Pager instance.
@@ -100,7 +70,7 @@ class Model
 	/**
 	 * Last insert ID
 	 *
-	 * @var integer
+	 * @var integer|string
 	 */
 	protected $insertID = 0;
 
