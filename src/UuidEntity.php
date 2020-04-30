a70476e591eeea05c9de22c1704e186c4b1e9b8f
michalsn <michal@sniatala.pl>
2020-04-30 07:50:41 +0200

    add UuidEntity

---
+++ src/UuidEntity.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Michalsn\UuidModel;
+
+use CodeIgniter\Entity;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Entity encapsulation, for use with Michalsn\UuidModel\UuidModel
+ */
+class UuidEntity extends Entity
+{
+	/**
+	 * Array of UUID fields that are stored in byte format.
+	 *
+	 * @param array
+	 */
+	protected $uuids = ['id'];
+
+	/**
+	 * Ensures our "original" values match the current values.
+	 *
+	 * @return $this
+	 */
+	public function syncOriginal()
+	{
+		$this->original = $this->attributes;
+
+		// Loop through the UUID array fields
+		foreach ($this->uuids as $uuid)
+		{
+			// Check if field is in byte format
+			if (mb_strlen($this->attributes[$uuid], 'UTF-8') < strlen($this->attributes[$uuid]))
+			{
+				$this->original[$uuid] = (Uuid::fromBytes($this->attributes[$uuid]))->toString();
+			}
+		}
+
+		return $this;
+	}
+}
